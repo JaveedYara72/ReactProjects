5901ce59c8048f9eacefb2f7c967b40914b86d7f
JaveedYara72 <javeedymd.iiitg@gmail.com>
2024-04-07 00:06:03 +0530

    restarted everything from scratch

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import reactDOM from "react-dom";
+
+const root = reactDOM.createRoot(document.getElementById("root"));
+
+const HeadingComponent = () => (
+  <>
+    <h1>Hello I am Javeed</h1>
+  </>
+);
+
+root.render(<HeadingComponent />);
