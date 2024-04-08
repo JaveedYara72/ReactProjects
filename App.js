d1778cf0e59e3e100fb6ae12441e0268aeb41cc1
JaveedYara72 <javeedymd.iiitg@gmail.com>
2024-04-08 11:04:03 +0530

    added images

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import React from "react";
-import reactDOM from "react-dom";
+import reactDOM from "react-dom/client";
 
 const root = reactDOM.createRoot(document.getElementById("root"));
 
-const HeadingComponent = () => (
-  <>
-    <h1>Hello I am Javeed</h1>
-  </>
-);
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <a href="">Amma's Restaurant</a>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-root.render(<HeadingComponent />);
+const Body = () => {
+  return (
+    <div className="Body">
+      <div className="Search"></div>
+      <div className="res-container"></div>
+    </div>
+  );
+};
+
+const AppComponent = () => {
+  return (
+    <div className="app">
+      <HeaderComponent />
+      <Body />
+    </div>
+  );
+};
+
+root.render(<AppComponent />);
